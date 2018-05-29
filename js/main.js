d3a8e13accdea93558e1fb8eda05d3dee1393509
Andrew Gallin <andrewgallin@gmail.com>
2018-05-29 14:38:51 -0400

    quarter circle button functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-const victory = 0;
+const victory = 20;
 let moves = 0;
+let gameState = false;
+let started = false;
+let finished = false;
+let circleButtons = ["green", "red", "blue", "yellow"]  //button order clockwise from top-left
+let soundSamples = ["https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
+					"https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
+					"https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
+					"https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"]
+
+function gameObj(state){
+	this.state = state;
+	this.changeState = function(){
+    	if(this.state){this.state = false;}
+    	else{this.state = true;}
+    }
+}
+
+function circleBtn(color, soundURL){
+	this.color = color;
+	this.soundURL = soundURL;
+
+	this.playSound = function(){
+		var audio = new Audio(this.soundURL);
+	}
+}
 
 function cntr(state, count) {
     this.state = state;  //bool False is off, true is on
@@ -17,26 +42,50 @@ function cntr(state, count) {
     }
 }
 
-
-
 $(document).ready(function(){
 
 	let countr = new cntr(false, 0);
+	let game = new gameObj(false);
 
 	$('.strict-toggle').click(function(e){
-		cycleLight();
+		if(!game.state){}
+		else{cycleLight()};
 	})
 
 	$('.slider').click(function(e){
-		cycleCount(countr);
+		if(!game.state){
+			initializeGame(countr, game);
+			game.changeState();	
+		}
+		else{
+			turnOff();
+			game.changeState();		
+		}
 	})
-
 });
+
+
+function initializeGame(countr, game){
+	$('.quarter').addClass("quarter-on");
+	cycleCount(countr);
+	displayCount(countr);
+	let circleBtnDict = {};
+	for (var i = 0; i < 4; i++) {
+		circleBtnDict[i] = new circleBtn(circleButtons[i], soundSamples[i])
+	}
+	console.log(circleBtnDict);
+
+	vict = initializeVictoryCondition();
+}
+
+function turnOff(){
+	$('.quarter').removeClass("quarter-on");
+}
 
 
 //geneartes the series of moves a user would need to make to win
 function initializeVictoryCondition(){
-
+	return Array.from({length: victory}, () => Math.floor(Math.random() * 4));
 }
 
 
@@ -63,3 +112,19 @@ function cycleCount(cntr){
 		$('.cnt-display').removeClass("led-off")
 	}
 }
+
+function displayCount(count){
+	let val = count.getCount();
+	if (val == 0){val = '--'}
+	val = pad(val, 2);
+	$('.cnt-display').text(val);
+}
+
+function pad(num, size) {
+    let s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+
+
